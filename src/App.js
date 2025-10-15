7f1866b0e1b5fed84c71dc8f3ce3897f69e06c66
Yegan Salehi <salehisediqe86@gmail.com>
2025-10-14 22:22:37 -0700

    Applied some colors and border

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="bg-green-500 text-  text-2xl p-4">
+    <div>
       
-      <h1 >سلام من سیما صالحی هستم</h1>
+      <h1 className="border-4 border-green-600" >سلام سیما صالحی هستم</h1>
        
     </div>
   );
