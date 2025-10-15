1702dc17f6ea32a04c186836c34320b3c931908d
Yegan Salehi <salehisediqe86@gmail.com>
2025-10-14 22:58:26 -0700

    Added screen size to tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html",
   ],
-  theme: {
+  theme: 
+  {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {},
   },
   plugins: [],
