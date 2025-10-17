8ccc04d2b33daed841985a05d9fa7fdbd9090add
Yegan Salehi <salehisediqe86@gmail.com>
2025-10-16 17:59:31 -0700

    styled footer by hovering by tailwind

--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@ import Home from "./Home";
 function App() {
   return (
     <div className="App">
-      <main>
+      
       <Home />
-      </main>
+     
       <footer className="flex justify-center space-x-1">
         <span className='text-sm'> Created by </span>
-        <a href='https://benevolent-druid-a3543f.netlify.app' className='text-sm'>Yegane Salehi </a>
       <span className='text-sm'> in </span>
-      <a href='https://github.com/sedi-yegan/attorney' className='text-sm'> Github</a>
+      <span className='text-sm'> in </span>
+        <a href='https://benevolent-druid-a3543f.netlify.app' 
+        target='_blank' rel='noopener noreferrer'
+        className='text-sm  text-blue-600 hover:underline '>
+            Yegane Salehi </a>
+      <span className='text-sm'> in </span>
+      <a href='https://github.com/sedi-yegan/attorney'
+      target='_blank' rel='noopener noreferrer'
+      className='text-sm text-blue-600 hover:underline'> Github</a>
       </footer>
     </div>
   );
