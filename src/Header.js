e98c6caf0cac9d39e005b77472594dedc6c8333e
Yegan Salehi <salehisediqe86@gmail.com>
2025-10-20 07:34:12 -0700

    Imported Scale symbol to Navbar

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Scale }from "lucide-react";
 
 export default function Header(){
     return(
@@ -8,21 +8,24 @@ export default function Header(){
  <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
 <nav class="relative">
   <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+
+    
+ <h1 className="flex justify-center items-center ">گروه حقوقی سایداد</h1>
     <div class="relative flex h-16 items-center justify-between">
-      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+     
 
        
-      </div>
+     
       <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
         <div class="flex shrink-0 items-center">
-          <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade-900" alt="Your Company" class="h-8 w-auto" />
+          <Scale className="text-blue-600 dark:text-blue-400" size={28}/>
         </div>
         <div class="hidden sm:ml-6 sm:block">
           <div class="flex space-x-4">
 
             {/* Current: "bg-gray-500 text-white", Default: "text-black hover:bg-white/5 hover:text-white" */}
 
-            <a href="#" aria-current="page" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Dashboard</a>
+            
             <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-white/5 hover:text-gray-500">Team</a>
             <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-white/5 hover:text-gray-500">Projects</a>
             <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-white/5 hover:text-gray-500">Calendar</a>
@@ -36,7 +39,7 @@ export default function Header(){
  
 </nav>
 
-        <h1 className="flex justify-center items-center ">گروه حقوقی سایداد</h1>
+       
      </div>
     );
 }
