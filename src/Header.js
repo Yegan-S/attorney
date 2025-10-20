e771bdf2850a8f976079ee1abd06d7db71958714
Yegan Salehi <salehisediqe86@gmail.com>
2025-10-20 06:46:39 -0700

    Updated Navbar by changing some properties

--- src/Header.js
+++ src/Header.js
@@ -15,17 +15,17 @@ export default function Header(){
       </div>
       <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
         <div class="flex shrink-0 items-center">
-          <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" class="h-8 w-auto" />
+          <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade-900" alt="Your Company" class="h-8 w-auto" />
         </div>
         <div class="hidden sm:ml-6 sm:block">
           <div class="flex space-x-4">
 
-            {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" */}
+            {/* Current: "bg-gray-500 text-white", Default: "text-black hover:bg-white/5 hover:text-white" */}
 
             <a href="#" aria-current="page" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Dashboard</a>
-            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Team</a>
-            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Projects</a>
-            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Calendar</a>
+            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-white/5 hover:text-gray-500">Team</a>
+            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-white/5 hover:text-gray-500">Projects</a>
+            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-white/5 hover:text-gray-500">Calendar</a>
           </div>
         </div>
       </div>
