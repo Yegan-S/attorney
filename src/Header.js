cc072b7953fb9a6a09276a07437c10bbc0cb8e42
Yegan Salehi <salehisediqe86@gmail.com>
2025-10-20 01:34:14 -0700

    tested Header component

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Header(){
     return(
      <div>
-        
+        <h1 className="flex justify-center items-center ">گروه حقوقی سایداد</h1>
      </div>
     );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Home.css"
-
+import Header from "./Header";
 
 
 export default function Home() {
     return (
         <div>
-    
-            <h1 className="flex justify-center items-center ">گروه حقوقی سایداد</h1>
+      <Header />
+            
+          
         </div>
     );
 }
