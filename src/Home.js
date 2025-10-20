0ba7346def50bdbc48e23989783ed79eb22b5363
Yegan Salehi <salehisediqe86@gmail.com>
2025-10-20 01:05:48 -0700

    made Header component

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,11 @@ import React from "react";
 import "./Home.css"
 
 
+
 export default function Home() {
     return (
         <div>
+    
             <h1 className="flex justify-center items-center ">گروه حقوقی سایداد</h1>
         </div>
     );
