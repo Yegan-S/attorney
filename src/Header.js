2d46f6a3a7fc4323a2e1fe2d90e8391f8aefa83b
Yegan Salehi <salehisediqe86@gmail.com>
2025-10-21 08:50:12 -0700

    Fixed some items in Navbar, and now Dark mode button is working

--- src/Header.js
+++ src/Header.js
@@ -4,41 +4,33 @@ import { Scale }from "lucide-react";
 export default function Header(){
     return(
      <div>
-    
- 
-<nav class="relative">
-  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <nav class="bg-gray-100 dark:bg-gray-900 border-b border-gray-300 dark:border-gray-700 shadow-sm">
 
-    
- 
-    <div class="relative flex h-16 items-center justify-between">
-     
+        <div class="mx-auto max-w-7xl px-4 py-3 flex items-center justify-between">
 
-       
-     
-      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        <div class="flex shrink-0 items-center">
-          <Scale className="text-blue-600 dark:text-blue-400" size={28}/>
-        </div>
-        <div class="hidden sm:ml-6 sm:block">
-          <div class="flex space-x-4">
+         
+           <div class="flex items-center space-x-2">
+           <Scale className="text-blue-600 dark:text-blue-400" size={28}/>
+          </div>
 
-            {/* Current: "bg-gray-500 text-white", Default: "text-black hover:bg-white/5 hover:text-white" */}
+          <h1 className="text-xl font-semibold text-gray-800 dark:text-gray-100 items-center "
+          >گروه حقوقی سایداد
+          </h1>
 
-            
-            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-white/5 hover:text-gray-500">Team</a>
-            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-white/5 hover:text-gray-500">Projects</a>
-            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-white/5 hover:text-gray-500">Calendar</a>
+            { /* Right Section -links */ }
+
+            <div class="flex space-x-4">
+
+            <a href="#" class=" text-gray-800 dark:text-gray-200  hover:text-blue-600 dark:hover:text-gray-400 text-sm font-medium">خانه</a>
+            <a href="#" class=" text-gray-800 dark:text-gray-200  hover:text-blue-600 dark:hover:text-gray-400 text-sm font-medium">خدمات</a>
+            <a href="#" class=" text-gray-800 dark:text-gray-200  hover:text-blue-600 dark:hover:text-gray-400 text-sm font-medium">تماس</a>
           </div>
         </div>
-      </div>
+     
     
-    </div>
-  </div>
+   
+  </nav>
 
- 
-</nav>
-<h1 className="flex justify-center items-center ">گروه حقوقی سایداد</h1>
        
      </div>
     );
