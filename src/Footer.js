e9bc7084cf5ba7fde7819f69c1e7af55f5c9ecc0
Yegan Salehi <salehisediqe86@gmail.com>
2025-10-23 08:21:07 -0700

    Updated some style in footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import { Github, MessageCircle} from "lucide-react";
 
 export default function Footer(){
     return( 
-<footer className="bg-gray-100 dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 py-4 px-4 flex flex-col  sm:flex-row justify-center items-center sm:space-x-2 space-y-1 sm:space-y-0 text-center text-sm ">
+<footer className="bg-gray-100 dark:bg-gray-600 border-t border-gray-200 dark:border-gray-700 py-4 px-4 flex flex-col  sm:flex-row justify-center items-center sm:space-x-2 space-y-1 sm:space-y-0 text-center text-sm ">
        <span className='text-sm'>
          @2025.All right is reserved.
          Created by 
@@ -20,13 +20,13 @@ export default function Footer(){
 
       <a href='https://github.com/Yegan-S'
          target='_blank' rel='noopener noreferrer'
-         className='text-sm text-blue-600 dark:text-blue-400 hover:text-green-800 dark:hover:text-white'>
+         className='text-sm text-blue-600 dark:text-blue-400 hover:text-orange-600 dark:hover:text-white transition'>
          <Github size={20}/>
       </a>
 
       <a href='https://wa.me/+989304195183'
          target='_blank' rel='noopener noreferrer'
-         className='text-sm text-blue-600 dark:text-blue-400 hover:text-green-800 dark:hover:text-white'>
+         className='text-sm text-blue-600 dark:text-blue-400 hover:text-orange-600 dark:hover:text-white transition'>
          <MessageCircle size={20}/>
       </a>
 
