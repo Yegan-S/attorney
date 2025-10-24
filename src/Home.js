5551d62909e3830f92fe8684c175f49234bdf919
Yegan Salehi <salehisediqe86@gmail.com>
2025-10-24 02:56:10 -0700

    Added img to the Home page

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ import "./Home.css"
 export default function Home() {
     return (
         <div >
-     
+     <img src="/images/sima.png" className=" rounded-full"></img>
 
       <p className="text-center text-4xl text-black py-4 "> This website is in progress</p>
         </div>
