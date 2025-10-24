1dc5aa50ed2711c423f3d2b6345a62bd75b8e447
Yegan Salehi <salehisediqe86@gmail.com>
2025-10-24 02:58:12 -0700

    Gave it fixed size and also centered it

--- src/Home.js
+++ src/Home.js
@@ -5,8 +5,8 @@ import "./Home.css"
 
 export default function Home() {
     return (
-        <div >
-     <img src="/images/sima.png" className=" rounded-full"></img>
+        <div className="flex justify-center" >
+     <img src="/images/sima.png" className="w-48 h-48 rounded-full"></img>
 
       <p className="text-center text-4xl text-black py-4 "> This website is in progress</p>
         </div>
