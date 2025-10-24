ab7cf297da512efb94fa3b8a179a6ac948b696b6
Yegan Salehi <salehisediqe86@gmail.com>
2025-10-24 03:15:48 -0700

    Centered it horizontally and vertically

--- src/Home.js
+++ src/Home.js
@@ -5,8 +5,13 @@ import "./Home.css"
 
 export default function Home() {
     return (
-        <div className="flex justify-center" >
+        <div>
+
+        <div className="flex justify-center items-center
+        
+        " >
      <img src="/images/sima.png" className="w-48 h-48 rounded-full"></img>
+          </div>
 
       <p className="text-center text-4xl text-black py-4 "> This website is in progress</p>
         </div>
