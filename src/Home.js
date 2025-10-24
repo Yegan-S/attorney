7bafbf1577289b31a8dfae43ba78c88e5500d587
Yegan Salehi <salehisediqe86@gmail.com>
2025-10-24 08:13:58 -0700

    Added some content and styled it

--- src/Home.js
+++ src/Home.js
@@ -7,10 +7,19 @@ export default function Home() {
     return (
         <div>
 
-        <div className="flex justify-center items-center
-        
-        " >
+        <div className="flex justify-center items-center py-2" >
      <img src="/images/sima.png" className="w-48 h-48 rounded-full"></img>
+     </div>
+     <div className=" rounded-full text-sm text-center">
+     <p >
+        .با رویکردی حرفه ای و دقیق, ایشان تمام تلاش خود را معطوف به حمایت از حقوق موکلین و ارایه راهکارهای موثر جهت حل و فصل سریع و مطلوب پرونده ها می نماید
+        <br></br>
+         .تعهد, تخصص و تجربه خانم صالحی در کنار دانش به روز حقوقی,ایشان را به انتخابی مطمین برای مسایل حقوقی پیچیده تبدیل کرده است
+         </p>
+         <br></br>
+             <p >
+                برای مشاوره حقوقی و همکاری های تخصصی, می توانید از طریق سایت یا شماره های اعلام شده با گروه سایداد در ارتباط باشید.
+          </p>
           </div>
 
       <p className="text-center text-4xl text-black py-4 "> This website is in progress</p>
