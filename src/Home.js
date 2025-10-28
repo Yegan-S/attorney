4b876c3a1e8c3fd79990de6e4d7e9ddfcfda9176
Yegan Salehi <salehisediqe86@gmail.com>
2025-10-28 09:54:21 -0700

    Made the paragraphs responsive in small screens

--- src/Home.js
+++ src/Home.js
@@ -12,18 +12,18 @@ export default function Home() {
         درباره ما         
     </h1>
 
-     <div className="flex justify-center items-center gap-6 py-4">
+     <div className="flex flex-col sm:flex-row justify-center items-center gap-6 py-4">
 
         <div className="flex justify-center items-center w-44 h-44  border-2 border-gray-400 dark:border-gray-600  rounded-full p-4 bg-white/80 dark:bg-black/40 shodow-md animate-fadeIn delay-300">
-             <p className="  text-sm text-center font-normal text-gray-800 dark:text-gray-100  leading-relaxed">
-            تعهد, تخصص و تجربه در کنار دانش به روز حقوقی, این گروه را به انتخابی مطمئن برای مسائل حقوقی پیچیده تبدیل کرده است
+             <p className="text-sm text-center font-normal text-gray-800 dark:text-gray-100  leading-relaxed">
+            موسسه حقوقی سایداد با تکیه بر تجربه, تخصص و تعهد, مجموعه ایی از خدمات جامع در حوزه مشاوره و وکالت را به اشخاص حقیقی و حقوقی ارائه می دهد
                  </p>
                   </div>
+                  
         <div className="flex justify-center items-center w-44 h-44  border-2 border-gray-400 dark:border-gray-600  rounded-full p-4 bg-white/80 dark:bg-black/40 shodow-md animate-fadeIn delay-300">
               <p className="text-sm text-center font-normal text-gray-800 dark:text-gray-100 leading-relaxed">
-         موسسه حقوقی سایداد با تکیه بر تجربه, تخصص و تعهد, مجموعه ایی از خدمات جامع در حوزه مشاوره و وکالت را به اشخاص حقیقی و حقوقی ارائه می دهد
-         
-         </p>
+                تعهد, تخصص و تجربه در کنار دانش به روز حقوقی, این گروه را به انتخابی مطمئن برای مسائل حقوقی پیچیده تبدیل کرده است. 
+                </p>
          </div>
         
         
